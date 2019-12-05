b7607dd3721eb19d08bf2f51fd9f53f78eab7b93
unknown <admir@misijaweb.com>
2019-12-05 12:18:06 +0100

    Added compact

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Project;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $projects = Project::orderBy('created_at', 'desc')->paginate(8);
+
+        return view('home', compact('projects'));
     }
 }
